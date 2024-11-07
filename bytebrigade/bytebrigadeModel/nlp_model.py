f2e22d09a6f95540842b7a1a3ed3ed02ed1a9456
SArora04 <sanyarora515@gmail.com>
2024-11-07 18:46:39 +0530

    semi-final build

--- bytebrigade/bytebrigadeModel/nlp_model.py
+++ bytebrigade/bytebrigadeModel/nlp_model.py
@@ -32,27 +32,84 @@ import os
 weight = 1.1
 predictions = ""
 actual_ls = ["Women/Child Related Crime","Financial Fraud Crimes","Other Cyber Crime"]
+map1 = ['Cyber Bullying  Stalking  Sexting',
+ 'Profile Hacking Identity Theft',
+ 'FakeImpersonating Profile',
+ 'RapeGang Rape RGRSexually Abusive Content',
+ 'Sexually Explicit Act',
+ 'Sexually Obscene material',
+ 'Child Pornography CPChild Sexual Abuse Material CSAM']
+map2 = ['Cryptocurrency Fraud',
+ 'EMail Phishing',
+ 'Online Matrimonial Fraud',
+ 'Online Job Fraud',
+ 'Fraud CallVishing',
+ 'UPI Related Frauds',
+ 'Internet Banking Related Fraud',
+ 'DebitCredit Card FraudSim Swap Fraud',
+ 'EWallet Related Fraud',
+ 'Business Email CompromiseEmail Takeover',
+ 'Online Gambling  Betting',
+ 'DematDepository Fraud']
 
-def class_label(x):
-    return actual_ls[x]
+map3 = ['Data Breach/Theft',
+ 'Hacking/Defacement',
+ 'Denial of Service (DoS)/Distributed Denial of Service (DDOS) attacks',
+ 'Malware Attack',
+ 'Ransomware Attack',
+ 'SQL Injection',
+ 'Tampering with computer source documents',
+ 'Cyber Terrorism',
+ 'Email Hacking',
+ 'Unauthorised AccessData Breach',
+ 'Damage to computer computer systems etc',
+ 'Impersonating Email',
+ 'Website DefacementHacking',
+ 'Cheating by Impersonation',
+ 'Intimidating Email',
+ 'Provocative Speech for unlawful acts',
+ 'Online Trafficking',
+ 'Ransomware',
+ 'Other']
 
-def category_classification(complaint):
-    print("Complain text :", complaint)
-    print("Current working directory : ", os.getcwd())
+def class_label(x, new_texts):
+    if x==0:
+        fname = "womenmodel"
+    elif x==1:
+        fname = "financialmodel"
+    else:
+        fname = "othermodel"
+        
+    model = keras.models.load_model(fname+'/tfidfNeural.h5')
+    tfidf_vectorizer = joblib.load(fname+'/tfidf_vectorizer.pkl')
+    selector = joblib.load(fname+'/selector.pkl')
+    scaler = joblib.load(fname+'/scaler.pkl')
+    svd = joblib.load(fname+'/svd.pkl')
+    new_tfidf = tfidf_vectorizer.transform([new_texts])
 
-    model = keras.models.load_model('model/3_class_tfidfNeural.h5')
-    tfidf_vectorizer = joblib.load('model/tfidf_vectorizer.pkl')
-    selector = joblib.load('model/selector.pkl')
-    svd = joblib.load('model/svd.pkl')
+    # Feature Selector
+    new_selected = selector.transform(new_tfidf)
 
-    new_sequences = tfidf_vectorizer.transform([complaint])
-    new_sequences_selected = selector.transform(new_sequences)
-    new_sequences_reduced = svd.transform(new_sequences_selected)
-    new_sequences_reduced = np.expand_dims(new_sequences_reduced, axis=2)
-    predictions = model.predict(new_sequences_reduced)
+    # StandardScaler
+    new_scaled = scaler.transform(new_selected)
 
-    return predictions
+    # SVD
+    new_reduced = svd.transform(new_scaled)
 
+    # Neural Network input reshape
+    new_input = np.expand_dims(new_reduced, axis=2)
+
+    # Prediction
+    predicted = model.predict(new_input)[0]
+    
+    predicted_class_index = int(np.where(predicted == np.max(predicted))[0])
+    if x==0:
+        return[actual_ls[x],map1[predicted_class_index]]
+    elif x==1:
+        return[actual_ls[x],map2[predicted_class_index]]
+    else:
+        return[actual_ls[x],map3[predicted_class_index]] 
+    
 def clean_text(text):
     text = str(text)
     '''Clean emoji, Make text lowercase, remove text in square brackets,remove links,remove punctuation
@@ -80,11 +137,29 @@ def clean_text(text):
     text = text.split()
     return " ".join(text)
 
+def category_classification(complaint):
 
-def sub_category_classification(complaint_text, predictions):
-    # for i in range(0,len(complaint_text)):
-    #     complaint_text[i] = clean_text(complaint_text[i])
-    complaint_text = [clean_text(text) for text in complaint_text]
+    cleaned_complaints = [clean_text(text) for text in complaint]
+
+    print("Tyep : ", type(cleaned_complaints))
+
+    model = keras.models.load_model('model/3_class_tfidfNeural.h5')
+    tfidf_vectorizer = joblib.load('model/tfidf_vectorizer.pkl')
+    selector = joblib.load('model/selector.pkl')
+    svd = joblib.load('model/svd.pkl')
+
+    new_sequences = tfidf_vectorizer.transform(cleaned_complaints)
+    new_sequences_selected = selector.transform(new_sequences)
+    new_sequences_reduced = svd.transform(new_sequences_selected)
+    new_sequences_reduced = np.expand_dims(new_sequences_reduced, axis=2)
+    predictions = model.predict(new_sequences_reduced)
+
+    return predictions
+
+def subcategory_classification(complaint_text, predictions):
+
+    cleaned_complaints = [clean_text(text) for text in complaint_text]
+
 
     tfidf_vectorizer = joblib.load('model2/tfidf_vectorizer.pkl')
     selector = joblib.load('model2/selector.pkl')
@@ -92,23 +167,109 @@ def sub_category_classification(complaint_text, predictions):
     svd = joblib.load('model2/svd.pkl')
     model = keras.models.load_model('model2/3_class_tfidfNeural.h5')
 
-    # TF-IDF Vectorizer
-    new_tfidf = tfidf_vectorizer.transform(complaint_text)
+    new_tfidf = tfidf_vectorizer.transform(cleaned_complaints)
+
     # Feature Selector
     new_selected = selector.transform(new_tfidf)
+
     # StandardScaler
     new_scaled = scaler.transform(new_selected)
+
     # SVD
     new_reduced = svd.transform(new_scaled)
+
     # Neural Network input reshape
     new_input = np.expand_dims(new_reduced, axis=2)
+
     # Prediction
     sec_predictions = model.predict(new_input)
-
     predicted = (predictions[0]*weight)+sec_predictions[0]
 
     predicted_class_index = int(np.where(predicted == np.max(predicted))[0])
-    result = class_label(predicted_class_index)
+    main_category, sub_category = class_label(predicted_class_index, complaint_text)
+    # predicted_categories = result.split(',')
 
-    return result
+    # main_category = predicted_categories[0]
+    # sub_category = predicted_categories[1]
+
+    return main_category, sub_category
+
+# def class_label(x):
+#     return actual_ls[x]
+
+# def category_classification(complaint):
+#     print("Complain text :", complaint)
+#     print("Current working directory : ", os.getcwd())
+
+#     model = keras.models.load_model('model/3_class_tfidfNeural.h5')
+#     tfidf_vectorizer = joblib.load('model/tfidf_vectorizer.pkl')
+#     selector = joblib.load('model/selector.pkl')
+#     svd = joblib.load('model/svd.pkl')
+
+#     new_sequences = tfidf_vectorizer.transform([complaint])
+#     new_sequences_selected = selector.transform(new_sequences)
+#     new_sequences_reduced = svd.transform(new_sequences_selected)
+#     new_sequences_reduced = np.expand_dims(new_sequences_reduced, axis=2)
+#     predictions = model.predict(new_sequences_reduced)
+
+#     return predictions
+
+# def clean_text(text):
+#     text = str(text)
+#     '''Clean emoji, Make text lowercase, remove text in square brackets,remove links,remove punctuation
+#     and remove words containing numbers.'''
+#     text = emoji.demojize(text)
+#     text = re.sub(r'\:(.*?)\:','',text)
+#     text = str(text).lower()    #Making Text Lowercase
+#     text = re.sub('\[.*?\]', '', text)
+#     #The next 2 lines remove html text
+#     text = BeautifulSoup(text, 'lxml').get_text()
+#     text = re.sub('https?://\S+|www\.\S+', '', text)
+#     text = re.sub('<.*?>+', '', text)
+#     text = re.sub('\n', '', text)
+#     text = re.sub('\w*\d\w*', '', text)
+#     specials = ["’", "‘", "´", "`"]
+#     for s in specials:
+#         text = text.replace(s, "'")
+#     # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",", "'")
+#     text = re.sub(r"[^a-zA-Z?.!,¿']+", " ", text)
+#     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+#     # eg: "he is a boy." => "he is a boy ."
+#     text = re.sub(r"([?.!,¿])", r" \1 ", text)
+#     text = re.sub(r'[" "]+', " ", text)
+#     text = text.strip()
+#     text = text.split()
+#     return " ".join(text)
+
+
+# def sub_category_classification(complaint_text, predictions):
+#     # for i in range(0,len(complaint_text)):
+#     #     complaint_text[i] = clean_text(complaint_text[i])
+#     complaint_text = [clean_text(text) for text in complaint_text]
+
+#     tfidf_vectorizer = joblib.load('model2/tfidf_vectorizer.pkl')
+#     selector = joblib.load('model2/selector.pkl')
+#     scaler = joblib.load('model2/scaler.pkl')
+#     svd = joblib.load('model2/svd.pkl')
+#     model = keras.models.load_model('model2/3_class_tfidfNeural.h5')
+
+#     # TF-IDF Vectorizer
+#     new_tfidf = tfidf_vectorizer.transform(complaint_text)
+#     # Feature Selector
+#     new_selected = selector.transform(new_tfidf)
+#     # StandardScaler
+#     new_scaled = scaler.transform(new_selected)
+#     # SVD
+#     new_reduced = svd.transform(new_scaled)
+#     # Neural Network input reshape
+#     new_input = np.expand_dims(new_reduced, axis=2)
+#     # Prediction
+#     sec_predictions = model.predict(new_input)
+
+#     predicted = (predictions[0]*weight)+sec_predictions[0]
+
+#     predicted_class_index = int(np.where(predicted == np.max(predicted))[0])
+#     result = class_label(predicted_class_index)
+
+#     return result
 
--- bytebrigade/bytebrigadeModel/views.py
+++ bytebrigade/bytebrigadeModel/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Complaint
-from .nlp_model import category_classification,sub_category_classification
+from .nlp_model import category_classification,subcategory_classification
 
 @csrf_exempt  # For simplicity, you can add CSRF protection if you're using forms
 def classify_complaint_view(request):
@@ -17,12 +17,12 @@ def classify_complaint_view(request):
         if complaint_text:
             # Classify the complaint using the NLP model
             prediction_modelResult = category_classification(complaint_text)
-            category_modelResult = sub_category_classification(complaint_text,prediction_modelResult)
+            category_modelResult, subcategory_modelResult = subcategory_classification(complaint_text,prediction_modelResult)
             # Save the complaint to the database
             #complaint = Complaint.objects.create(complaint_text=complaint_text, category=category)
 
             # Return the result as JSON
-            return JsonResponse({'status': 'success', 'category': category_modelResult, 'sub-category': 'NA'})
+            return JsonResponse({'status': 'success', 'category': category_modelResult, 'sub-category': subcategory_modelResult})
 
         return JsonResponse({'status': 'error', 'message': 'No complaint text provided.'})
 
